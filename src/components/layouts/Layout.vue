afd1d289798adf128df020c9559db0cb44932f2d
MrTizu <99123684+tizul@users.noreply.github.com>
2023-11-19 20:28:42 +0200

    Update Layout.vue

--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -2,19 +2,18 @@
   <div class="layout">
     <Navbar ref="Navbar"/>
     <router-view class="main"/>
-    <FooterPanel ref="FooterPanel"/>
+
   </div>
 </template>
 
 <script>
   import Navbar from '@/components/layouts/Navbar';
-  import FooterPanel from '@/components/layouts/FooterPanel';
 
   export default {
     name: 'Layout',
     components: {
       Navbar,
-      FooterPanel
+  
     }
   }
 </script>
